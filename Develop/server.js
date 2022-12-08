0203c942aa0a09428fb120560ad277fd87a4fc55
EfSoren <112333698+EfSoren@users.noreply.github.com>
2022-12-07 17:06:15 -0700

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded( { extended: true}));
 
-
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public/index.html'))})
 app.get('/notes', (req, res) => { res.sendFile(path.join(__dirname, 'public/notes.html'))})
 app.get('/api/notes', (req, res) => { res.json(noteData)})
 app.post('/api/notes', (req, res) => {
